39289e59346d87cc1c8e7f775aef77ce05e9e1de
leeway64 <lee.way56@comcast.net>
2023-03-01 18:56:35 -0800

    Use std::numbers::pi instead of self-defined value for pi
    - Explicity set C++ version to C++20 with set in CMakeLists

--- src/LWDSP.hpp
+++ src/LWDSP.hpp
@@ -7,9 +7,8 @@
 
 #include <complex>
 #include <vector>
+#include <numbers>
 
-
-const double pi = 2 * acos(0.0);
 const std::complex<double> j(0,1);
 
 
@@ -125,12 +124,14 @@ LWDSP& LWDSP::operator||(const LWDSP& other) {
     std::vector<double> largest = this->h;
     std::vector<double> smallest = other.h;
 
-    if (other.h.size() > this->h.size()) {
+    if (other.h.size() > this->h.size())
+    {
         largest = other.h;
         smallest = this->h;
     }
 
-    for (auto n = 0; n < smallest.size(); ++n) {
+    for (auto n = 0; n < smallest.size(); ++n)
+    {
         largest[n] = smallest[n] + largest[n];
     }
 
@@ -143,39 +144,17 @@ LWDSP& LWDSP::operator||(const LWDSP& other) {
 // Returns the DFT of the input vector. Output is a vector input.size() long.
 std::vector<std::complex<double>> LWDSP::DFT(std::vector<double> input) {
     std::vector<std::complex<double>> result;
-    const int inputSize = input.size();
-    for (int k = 0; k < inputSize; k++)
+    const int N = input.size();
+    for (int k = 0; k < N; k++)
     {
-        // result.push_back(DFTElement(input, angularFrequency(inputSize), i));
         std::complex<double> element(0, 0);
-        for (int n = 0; n < inputSize; ++n)
+        for (int n = 0; n < N; ++n)
         {
-            double knN = 1.0 * k * n / inputSize;
-            element += exp(-1.0 * j * 2.0 * pi * knN) * input[n];
+            const double knN = 1.0 * k * n / N;
+            element += exp(-1.0 * j * 2.0 * std::numbers::pi * knN) * input[n];
         }
         result.push_back(element);
     }
     return result;
 }
-/*
-// Calculate each DFT value one by one. Helper function for the DFT function.
-std::complex<double> LWDSP::DFTElement(std::vector<double> input, std::vector<double> angularFrequency, int k) {
-    std::complex<double> result(0, 0);
-    int inputLength = input.size();
-    for (int i = 0; i < inputLength; i++) {
-        result += std::polar(1.0 * abs(input[i]), (-1.0 * angularFrequency[i] * k) + std::arg(input[i]));
-    }
-    return result;
-}
-
-// Helper function for the DFT function.
-std::vector<double> LWDSP::angularFrequency(int inputSize) {
-    const double x = 2 * pi / inputSize;
-    std::vector<double> result;
-    for (int i = 0; i < inputSize; i++) {
-        result.push_back(x * i);
-    }
-    return result;
-}*/
-
 #endif // DSP_H
